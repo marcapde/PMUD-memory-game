ddb4bafd850ad5eba6c08125392ab79ad846f009
Marc Capde <capasc5@gmail.com>
2022-10-13 19:20:55 +0200

    added chrono + move counter

--- js/jquery.js
+++ js/jquery.js
@@ -1,17 +1,52 @@
 $(function(){
     let pairs = [];
+    let nCard = 12;
+    let moveCount = 0;	
+    let startTime = 0;
+    let actTime = 0;
+    let finishCrono=false;
         // document.getElementById("btnStart").remove();
     startGame = (n) => {
         // document.getElementById("btnStart").remove();
-        document.getElementsByClassName("start_container")[0].style.top="-1000vh";
+        $(".start_container").css("top","-1000vh");
+
+        nCard = n;
+        iniStats();
         iniTable(n);
     }
-       
-    function endGame(n) {
+    iniStats = () =>{
+        start  = new Date();
+        moveCount=0;
+        finishCrono=false
+        setChrono();
+        setMoves();
+    }
+    setChrono = () =>{
+        actTime = new Date();
+        let diff = actTime - start;
+        diff = new Date(diff);
+        let ms = diff.getMilliseconds();
+        let mn = diff.getMinutes();
+        let sc = diff.getSeconds();
+        let hr = diff.getHours() - 1 ;
+
+        ms = ms < 10 ? '0' + ms : ms;
+        sc = sc < 10 ? '0' + sc : sc;
+        mn = mn < 10 ? '0' + mn : mn;
+        hr = hr < 10 ? '0' + hr : hr;
+        let content = `<p>Your time: ${hr} : ${mn} : ${sc} : ${ms}</p>`;
+        $('#chrono').html(content);
+        if(!finishCrono)setTimeout(setChrono,10);
+    }   
+    setMoves = () => {
+        let content = `<p>Your move nº:  ${moveCount}</p>`;
+        $('#moves').html(content);
+    } 
+    function endGame() {
         let finished = true;
-        for (let i = 0; i < n; i++) {
+        for (let i = 0; i < nCard; i++) {
             let card = $(`#card${i}`);
-            // console.log(`card${i}`);
+            console.log(card);
             if (!card.hasClass("correct")){
                 finished = false;
                 break;
@@ -19,6 +54,7 @@ $(function(){
         }
         console.log("finished:", finished);
         if (finished){
+            finishCrono=true;
             $(".start_container").css("top","20%");
         } 
     }
@@ -79,6 +115,8 @@ $(function(){
         if (card.hasClass("hidden")){            
             card.removeClass("hidden");
             if(pairs.length==2){
+                moveCount += 1;
+                setMoves();
                 if(!keep){
                     setTimeout(hideImage,1000,pairs[0]);
                     setTimeout(hideImage,1000,pairs[1]);
@@ -91,7 +129,7 @@ $(function(){
         else if(!keep) {
             card.addClass("hidden");
         }
-        endGame(12);
+        endGame();
     }
     function hideImage(imgId) {
         let card = $(`#${imgId}`)
